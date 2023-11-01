7b44d572d35922611ae1fa6242dd5da4c63bda31
Enzo Cipriani <egcipriani@hotmail.com>
2023-11-01 18:32:38 -0300

    dadabase_firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAd-O6SVTuKL2xVdnybxO0QKSmKjIDIb0",
+    authDomain: "casamento-leticia.firebaseapp.com",
+    projectId: "casamento-leticia",
+    storageBucket: "casamento-leticia.appspot.com",
+    messagingSenderId: "211568741009",
+    appId: "1:211568741009:web:c51ea1ee048fa938d74d30",
+    measurementId: "G-XZNZQYGS94"
+};
+
+export const app = initializeApp(firebaseConfig);
---
+++ index.js
@@ -0,0 +1,56 @@
+import { app } from "./firebaseConfig.js";
+import { getDatabase, set, ref, onValue } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js";
+
+const database = getDatabase(app);
+
+const invitesRef = ref(database, 'invites');
+
+const button = document.getElementById('button');
+
+var invitesData;
+var confirmedData;
+
+onValue(ref(database, 'invites/'), (snapshot) => {
+    invitesData = snapshot.val();
+    console.log(invitesData);
+});
+onValue(ref(database, 'confirmados/'), (snapshot) => {
+    confirmedData = snapshot.val();
+    console.log(confirmedData);
+});
+
+button.addEventListener('click', function() {
+    
+    const name = document.getElementById('name').value;
+    const lastname = document.getElementById('lastname').value;
+    const tel = document.getElementById('tel').value;
+    const uid = document.getElementById('uid').value;
+    const getName = name.toLowerCase();
+
+    const inviteName = invitesData[uid];
+    const confirmedNumber = confirmedData[uid];
+
+    if(inviteName == getName) {
+
+        if(confirmedNumber !== undefined) {
+
+            alert('CONVITE JÁ CONFIRMADO!')
+
+        } else {
+
+            //set(ref(database, 'confirmados/' + uid), name + ' ' + lastname);
+            set(ref(database, 'confirmados/' + uid), {
+                convidado: name + ' ' + lastname,
+                telefone: tel,
+                convite: uid
+            });
+
+        }
+
+    } else {
+
+        alert('CONVITE INVÁLIDO!')
+
+    }
+
+});
