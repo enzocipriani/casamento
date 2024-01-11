2ff4cbbbcef719e2d6c0ab6fb809656b412c5cee
Enzo Cipriani <egcipriani@hotmail.com>
2024-01-11 18:30:39 -0300

    edit_presenca

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function menuShow() {
         document.querySelector('.icon').src = "assets/close/close.svg"
     }
 }
+
+function myFunction(x) {
+    var tooltipText = x.getElementsByClassName("tooltiptext")[0];
+    if (tooltipText.style.visibility === "hidden") {
+      tooltipText.style.visibility = "visible";
+      tooltipText.style.opacity = 1;
+      setTimeout(function() {
+        tooltipText.style.visibility = "hidden";
+        tooltipText.style.opacity = 0;
+      }, 5000); 
+    } else {
+      tooltipText.style.visibility = "hidden";
+      tooltipText.style.opacity = 0;
+    }
+  }
