23843ac536d26fb830efa67f89d8c6fda40ce994
Enzo Cipriani <egcipriani@hotmail.com>
2024-05-27 18:21:12 -0300

    teste4video

--- video.js
+++ video.js
@@ -11,7 +11,6 @@ document.getElementById('videoInput').addEventListener('change', (event) => {
         video.src = URL.createObjectURL(file);
         video.style.display = 'block';
         document.getElementById('uploadButton').style.display = 'block';
-
         document.getElementById('uploadButton').onclick = () => {
             uploadVideoToFirebase(file);
         };
@@ -21,13 +20,13 @@ document.getElementById('videoInput').addEventListener('change', (event) => {
 async function uploadVideoToFirebase(file) {
     try {
         console.log('Iniciando upload do vídeo');
-        alert('Iniciando upload do vídeo');
+        document.getElementById('loader').style.display = 'block'; // Mostra o loader durante o upload
         const storageRef = ref(storage, 'videos/' + new Date().toISOString() + '.' + file.name.split('.').pop());
         const uploadTaskSnapshot = await uploadBytes(storageRef, file);
-        
-    
+        document.getElementById('loader').style.display = 'none'; // Esconde o loader após o upload
         alert('Vídeo enviado com sucesso!');
     } catch (error) {
         alert('Erro ao fazer upload do vídeo.');
+        document.getElementById('loader').style.display = 'none'; // Em caso de erro, esconde o loader também
     }
 }
